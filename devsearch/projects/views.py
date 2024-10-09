eb86cbe2933927f1d5c9ee906f812679350633e3
Daithí <davecoll3@gmail.com>
2024-10-09 18:51:03 +0000

    created projects.html and single-project.html templates

--- devsearch/projects/views.py
+++ devsearch/projects/views.py
@@ -1,9 +1,8 @@
 from django.shortcuts import render
-from django.http import HttpResponse
 
 def projects(request):
-    return HttpResponse('Here are our projects')
+    return render(request, 'projects.html')
 
 def project (request, pk):
-    return HttpResponse('Here is project' + ' ' + str(pk))
+    return render(request, 'single-project.html')
 
